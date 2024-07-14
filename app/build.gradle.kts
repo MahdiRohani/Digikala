c252872d2133b4f19ecd8fcb8574e110c1e64db9
SMR62434 <m.rohani1381@gmail.com>
2024-07-15 01:15:25 +0330

    Add-dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
 }
 
 android {
@@ -50,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
@@ -66,4 +67,42 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    //room
+    implementation("androidx.room:room-runtime:2.3.0")
+    kapt("androidx.room:room-compiler:2.3.0")
+    implementation("androidx.room:room-ktx:2.3.0")
+
+    //datastore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //hilt di
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    //compose navigation
+    implementation("androidx.navigation:navigation-compose:2.6.0-alpha04")
+
+    //animation
+    implementation("com.airbnb.android:lottie-compose:5.2.0")
+
+    //coil - load image from url
+    implementation("io.coil-kt:coil-compose:2.0.0-rc01")
+
+    //swipe refresh
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.27.0")
+
+    //system ui controller
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
