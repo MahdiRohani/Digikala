4995e7cecb9696bcd2d81207b17ac2654dacb67c
SMR62434 <m.rohani1381@gmail.com>
2024-07-18 21:27:49 +0330

    SplashScreen_Design

--- app/src/main/java/com/project/digikala/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/project/digikala/ui/screens/SplashScreen.kt
@@ -1,11 +1,73 @@
 package com.project.digikala.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.project.digikala.R
+import com.project.digikala.navigation.Screen
+import com.project.digikala.ui.components.Loading3Dots
+import com.project.digikala.ui.theme.splashBg
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(navController: NavHostController){
-    Text(text = "SplashScreen")
+fun SplashScreen(navController: NavHostController) {
+    Splash()
+    LaunchedEffect(key1 = true){
+        delay(2500)
+        navController.navigate(Screen.Home.route)
+    }
 
+}
+
+
+@Composable
+fun Splash() {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.splashBg)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.size(250.dp),
+            painter = painterResource(id = R.drawable.digi_logo),
+            contentDescription = null
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(100.dp),
+            contentAlignment = Alignment.BottomCenter
+
+        ) {
+            Image(
+                modifier = Modifier.height(30.dp),
+                painter = painterResource(id = R.drawable.digi_txt_white),
+                contentDescription = null
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            contentAlignment = Alignment.BottomCenter
+
+        ) {
+            Loading3Dots(isDark = false)
+        }
+
+    }
 }
--- app/src/main/java/com/project/digikala/ui/theme/Color.kt
+++ app/src/main/java/com/project/digikala/ui/theme/Color.kt
@@ -1,5 +1,9 @@
 package com.project.digikala.ui.theme
 
+
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +13,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Purple200 = Color(0xFFed1b34)
+val Purple500 = Color(0xFFCE3C4D)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+
+val ColorScheme.splashBg : Color
+    @Composable
+    get() = Color(0xFFed1b34)
