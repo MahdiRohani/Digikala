910ccd0adf31895bcf5e4b770f3630c3d1ef3859
SMR62434 <m.rohani1381@gmail.com>
2024-07-24 14:52:56 +0330

    safe-api-call

---
+++ app/src/main/java/com/project/digikala/data/remote/BaseApiResult.kt
@@ -0,0 +1,30 @@
+package com.project.digikala.data.remote
+
+import com.project.digikala.data.model.ResponseResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+
+abstract class BaseApiResult {
+
+    suspend fun <T> safeApiCall(apiCall : suspend ()-> Response<ResponseResult<T>>) : NetworkResult<T> =
+        withContext(Dispatchers.IO){
+            try {
+                val response = apiCall()
+                if (response.isSuccessful){
+                    val body = response.body()
+                    body?.let {
+                        return@withContext NetworkResult.Success(body.message,body.data)
+                    }
+                }
+                return@withContext error("code is ${response.code()}, message : ${response.message()}")
+            }catch (e : Exception){
+                return@withContext error(e.message?: e.toString())
+
+            }
+        }
+
+
+    private fun <T> error(errorMessage : String) : NetworkResult<T> =
+        NetworkResult.Error("Api call failed : $errorMessage")
+}
