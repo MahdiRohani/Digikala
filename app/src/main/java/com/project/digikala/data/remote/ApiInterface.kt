3d3b7733163aeea58ce1e3a17e28b50ffb2706d8
SMR62434 <m.rohani1381@gmail.com>
2024-07-24 11:48:18 +0330

    NetworkModule

---
+++ app/src/main/java/com/project/digikala/data/remote/ApiInterface.kt
@@ -0,0 +1,5 @@
+package com.project.digikala.data.remote
+
+interface ApiInterface {
+
+}
--- app/src/main/java/com/project/digikala/util/Constants.kt
+++ app/src/main/java/com/project/digikala/util/Constants.kt
@@ -3,6 +3,8 @@ package com.project.digikala.util
 object Constants {
     const val ENGLISH_LANG = "en"
     const val PERSIAN_LANG = "fa"
+    const val TIMEOUT_IN_SECOND : Long = 60
     const val DATASTORE_NAME = "DIGIKALA_DATA_STORE"
+    const val BASE_URL = ""
     var USER_LANGUAGE = "USER_LANGUAGE"
 }
